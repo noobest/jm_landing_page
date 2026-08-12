cae605c0dfd84c4d1650d419553bc2baa845102b
Vanessa Ibale <vanessaibale@Vanessas-MacBook-Air-2.local>
2026-08-13 03:37:44 +0800

    feat: hero section

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -18,11 +18,10 @@ const Header = () => {
           <img src="/images/xora.svg" width={155} height={55} alt="logo" />
         </a>
 
-
         <div
           className={clsx("w-full max-lg:fixed max-lg:top-0 max-lg:left-0 max-lg:w-full max-lg:bg-s2 max-lg:opacity-0", isOpen ? 'max-lg:opacity-100':'max-lg:pointer-events-none')}>
           <div
-            className='w-full max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg"overflowhidden sidebar-before
+            className='max-lg:relative max-lg:flex max-lg:flex-col max-lg:min-h-screen max-lg"overflowhidden sidebar-before
             max-md:px-4'
           >
             <nav className="max-lg:relative max-lg:z-2 max-lg:my-auto">
