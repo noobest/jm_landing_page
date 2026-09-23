4e31578b2c4245012ecee3259c2de025bad98fc0
Vanessa Ibale <bamnb39@gmail.com>
2026-09-23 22:17:28 +0800

    feat: FaqItem

--- src/sections/Faq.jsx
+++ src/sections/Faq.jsx
@@ -30,7 +30,7 @@ const Faq = () => {
 
 						<div className="relative flex-1 p-24">
 							{faq.slice(0, halfLength).map((item, index) => (
-								<FaqItem key={item.id} faq={item} index={index} />
+								<FaqItem key={item.id} item={item} index={index} />
 							))}
 						</div>
 					</div>
