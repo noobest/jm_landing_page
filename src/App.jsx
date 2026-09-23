3ecb56c05835d8939ff9744c29259cdc21ed06f2
Vanessa Ibale <bamnb39@gmail.com>
2026-09-23 22:46:49 +0800

    feat:testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Header from './sections/Header'
-import Hero from './sections/Hero'
-import Features from './sections/Features'
-import Pricing from './sections/Pricing'
-import Faq from './sections/Faq'
+import React from "react";
+import Header from "./sections/Header";
+import Hero from "./sections/Hero";
+import Features from "./sections/Features";
+import Pricing from "./sections/Pricing";
+import Faq from "./sections/Faq";
+import Testimonials from "./sections/Testimonials";
 
 const App = () => {
-  return (
-   <main className='overflow-hidden'>
-      <Header/>
-      <Hero/>
-      <Features/>
-      <Pricing />
-      <Faq />
-   </main>
-  )
-}
+	return (
+		<main className="overflow-hidden">
+			<Header />
+			<Hero />
+			<Features />
+			<Pricing />
+			<Faq />
+			<Testimonials />
+		</main>
+	);
+};
 
-export default App
+export default App;
