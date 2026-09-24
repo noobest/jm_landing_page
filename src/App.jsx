05243b62d5e3e761b531fadffd6cabf895dc28ec
Vanessa Ibale <bamnb39@gmail.com>
2026-09-25 02:55:00 +0800

    feat: downloads section

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Features from "./sections/Features";
 import Pricing from "./sections/Pricing";
 import Faq from "./sections/Faq";
 import Testimonials from "./sections/Testimonials";
+import Downloads from "./sections/Downloads";
 
 const App = () => {
 	return (
@@ -15,6 +16,7 @@ const App = () => {
 			<Pricing />
 			<Faq />
 			<Testimonials />
+			<Downloads />
 		</main>
 	);
 };
